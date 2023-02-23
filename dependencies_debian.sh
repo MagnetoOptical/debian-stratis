544180fe9e087b3f66a20cd0efd5aca4186f8de0
MagnetoOptical <4e9b58@projectceladon.org>
2023-02-23 12:16:33 -0600

    Add some code to build correctly

--- dependencies_debian.sh
+++ dependencies_debian.sh
@@ -2,51 +2,33 @@
 
 # Install dependencies for building and testing Stratis on Debian.
 # Packages are assumed to be for your native architecture.
-# For clarity, packages are grouped into sections:
-# - Base build tools (git, make)
-# - Base toolchain (compilers and associated libraries)
-# - Core library dependencies (D-Bus, device-mapper, systemd, etc.)
-# - Python 3 dependences
-apt -y install git \
-	make \
-	clang \
-	gcc \
-	libglib2.0-dev
-	llvm-11 \
-	llvm-11-dev \
-	libudev-dev \
-	\
-	asciidoc \
-	clevis \
-	clevis-luks \
-	libcryptsetup-dev \
-	libdbus-1-dev \
-	python3-dbus \
-	libssl-dev \
-	python3-dbus \
-	libsystemd-dev \
-	thin-provisioning-tools \
-	xfsprogs \
-	\
-	python3-pip
-	python3-venv
-	python3-coverage \
-	python3-dateutil \
-	python3-packaging \
-	python3-psutil \
-	python3-pyparsing \
-	python3-pyudev \
-	python3-semantic-version \
-	python3-setuptools \
-	python3-wcwidth
+sudo apt -y install git make clang gcc libglib2.0-dev llvm-11 llvm-11-dev libudev-dev \
+        curl asciidoc clevis clevis-luks libcryptsetup-dev libdbus-1-dev \
+	python3-dbus libssl-dev python3-dbus libsystemd-dev \
+	thin-provisioning-tools xfsprogs python3-pip python3-venv python3-coverage \
+	python3-dateutil python3-packaging python3-psutil python3-pyparsing \
+	python3-pyudev 	python3-semantic-version python3-setuptools \
+       	python3-wcwidth python3-dbus
 
 # Needs logic control so this variable declaration isn't duplicated on successive runs
-echo "export PATH=$HOME/.local/bin:$HOME/.cargo/bin" >> $HOME/.bashrc
+echo "export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.cargo/env:$PATH" >> $HOME/.bashrc
 source $HOME/.bashrc
 
-# Official Rust install one-liner
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Derived from the official Rust install one-liner
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
+chmod +x rustup.sh
+./rustup.sh -q --profile default
 
 python3 -m venv venv
-. venv/bin/activate
+source venv/bin/activate
 python3 -m pip install justbytes
+
+git clone https://github.com/stratis-storage/stratisd.git
+cd stratisd
+export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.cargo/env:$PATH
+make build-all
+sudo make install
+cd ..
+git clone https://github.com/stratis-storage/stratis-cli.git
+cd stratis-cli
+python3 setup.py install
