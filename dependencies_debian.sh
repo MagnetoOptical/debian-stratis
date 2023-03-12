00d1c1ad9e09e432f06beae72fb70eb39f056c8f
S. Seago <6930610+MagnetoOptical@users.noreply.github.com>
2023-03-12 16:04:27 -0500

    Update dependencies_debian.sh

--- dependencies_debian.sh
+++ dependencies_debian.sh
@@ -3,8 +3,9 @@
 # Install dependencies for building and testing Stratis on Debian.
 # Packages are assumed to be for your native architecture.
 sudo apt -y install git make clang gcc libglib2.0-dev llvm-11 llvm-11-dev libudev-dev \
-        curl asciidoc clevis clevis-luks libcryptsetup-dev libdbus-1-dev \
+        curl asciidoc clevis clevis-luks libcryptsetup-dev libdbus-1-dev rustc cargo\
 	python3-dbus libssl-dev python3-dbus libsystemd-dev \
+	python3-jsonschema, python3-pydbus, python3-pycparser, python3-sysv_ipc, python3-setuptools
 	thin-provisioning-tools xfsprogs python3-pip python3-venv python3-coverage \
 	python3-dateutil python3-packaging python3-psutil python3-pyparsing \
 	python3-pyudev 	python3-semantic-version python3-setuptools \
@@ -14,10 +15,11 @@ sudo apt -y install git make clang gcc libglib2.0-dev llvm-11 llvm-11-dev libude
 echo "export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.cargo/env:$PATH" >> $HOME/.bashrc
 source $HOME/.bashrc
 
+# NOTE: It appears only rustc and cargo are needed; those can be installed via apt
 # Derived from the official Rust install one-liner
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-chmod +x rustup.sh
-./rustup.sh -q --profile default
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
+#chmod +x rustup.sh
+#./rustup.sh -q --profile default
 
 python3 -m venv venv
 source venv/bin/activate
